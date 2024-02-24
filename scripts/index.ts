bad1f1fc094133591739f7917cd6f866499dffb9
virtualp1 <zininnikita309@gmail.com>
2024-02-24 16:18:54 +0900

    init

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,4 +1,7 @@
 const TIMEOUT_TO_PLAY_VIDEO = 5 * 1000
+const SECONDS_BEFORE_END_TO_TRIGGER_NEXT_EPISODE = 30
+const MIN_VIDEO_DURATION_FOR_TRIGGER = 10
+const DEFAULT_EPISODES_PER_SEASON = 24
 
 const EPISODES_IN_SEASON = {
   1: 17,
@@ -7,8 +10,7 @@ const EPISODES_IN_SEASON = {
 }
 
 function getParams() {
-  const search = new URL(location.href).search
-  const searchParams = new URLSearchParams(search)
+  const searchParams = new URLSearchParams(window.location.search)
 
   return {
     season: Number(searchParams.get('season')),
@@ -16,23 +18,26 @@ function getParams() {
   }
 }
 
-function getInfo() {
-  let { season, episode } = getParams()
-
-  const lastEpisode = EPISODES_IN_SEASON[season] || 24
+function getNextEpisodeInfo(season: number, episode: number) {
+  const lastEpisode = EPISODES_IN_SEASON[season] || DEFAULT_EPISODES_PER_SEASON
   const isLastEpisode = lastEpisode === episode
 
-  if (isLastEpisode) {
-    season += 1
-  }
-
   return {
-    season,
-    episode: isLastEpisode ? 1 : episode,
+    season: isLastEpisode ? season + 1 : season,
+    episode: isLastEpisode ? 1 : episode + 1,
   }
 }
 
-function init() {
+function goToNextEpisode(season: number, episode: number) {
+  const { season: nextSeason, episode: nextEpisode } = getNextEpisodeInfo(
+    season,
+    episode,
+  )
+
+  location.href = `https://plplayer.online/s/486?season=${nextSeason}&episode=${nextEpisode}&voice=14&voonly=true`
+}
+
+function initializeVideoPlayer() {
   const videoPlayer = document.querySelector('video')
 
   if (!videoPlayer) {
@@ -45,12 +50,16 @@ function init() {
     const videoCurrentTime = videoPlayer.currentTime
     const timeToVideoEnd = videoDuration - videoCurrentTime
 
-    if (timeToVideoEnd <= 30 && videoDuration > 10) {
-      const { season, episode } = getInfo()
+    if (
+      timeToVideoEnd <= SECONDS_BEFORE_END_TO_TRIGGER_NEXT_EPISODE &&
+      videoDuration > MIN_VIDEO_DURATION_FOR_TRIGGER
+    ) {
+      const { season, episode } = getParams()
 
-      location.href = `https://plplayer.online/s/486?season=${season}&episode=${episode + 1}&voice=14&vonly=true`
+      goToNextEpisode(season, episode)
     }
   }, 1000)
 }
 
-setTimeout(init, TIMEOUT_TO_PLAY_VIDEO)
+// Wait for TIMEOUT_TO_PLAY_VIDEO before initializing
+setTimeout(initializeVideoPlayer, TIMEOUT_TO_PLAY_VIDEO)
