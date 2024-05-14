4915f6c1bbc54f6f4ad19c6ede91602aebd4e9c0
virtualp1 <zininnikita309@gmail.com>
2024-05-14 20:11:27 +0900

    conditions

--- scripts/index.ts
+++ scripts/index.ts
@@ -2,23 +2,42 @@ function removeDescription() {
   const descriptionElement = document.querySelector(
     'span[itemprop=description]',
   )
-  descriptionElement.remove()
+
+  if (descriptionElement) {
+    descriptionElement.remove()
+  }
 
   const titles = document.querySelectorAll('.entry-title-h2')
-  // The title of the "Description" block has an index of 2
-  titles[2].remove()
+
+  if (titles.length > 2) {
+    // The title of the "Description" block has an index of 2
+    titles[2].remove()
+  }
 }
 
 function setPlayerToFullWidth() {
   const sidebar = document.querySelector('.sidebar')
-  sidebar.remove()
+
+  if (sidebar) {
+    sidebar.remove()
+  }
 
   const content: HTMLDivElement = document.querySelector('.content')
+
+  if (!content) {
+    return
+  }
+
   content.style.width = 'auto'
 }
 
 function removePageLink() {
   const branding: HTMLLinkElement = document.querySelector('.branding')
+
+  if (!branding) {
+    return
+  }
+
   branding.removeAttribute('href')
 }
 
